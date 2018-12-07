eb8b0d793a916255f8eb5f8b101b15b3d2ad536d
Adrian de la Rosa <adrian@delarosab.me>
2018-12-07 17:09:47 +0100

    :white_check_mark: Adding jest

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  collectCoverageFrom: ['src/**/*.js'],
+  coverageDirectory: '<rootDir>/coverage',
+  setupFiles: [
+    './lib/jest/shim.js',
+    './lib/jest/enzyme.js',
+    './lib/jest/polyfill.js',
+  ],
+  testMatch: ['<rootDir>/test/**/*.test.js'],
+};
