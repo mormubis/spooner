301be634bed90c72f2c5d67c91744e1c803782d1
Adrian de la Rosa <adrian@delarosab.me>
2019-03-22 22:33:41 +0100

    :bug: Fixing modifiable value

--- src/Field.js
+++ src/Field.js
@@ -1,7 +1,7 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import compose from 'underscore-es/compose';
 
+import Connector from './Connector';
 import { Consumer, Provider } from './Form';
 import withControlledProp from './with/controlledProp';
 
@@ -11,51 +11,6 @@ function identity(children) {
 
 function withProvider(children) {
   return <Provider>{children}</Provider>;
-}
-
-export class Connector extends PureComponent {
-  static defaultProps = {
-    onChange() {},
-    set() {},
-    unset() {},
-  };
-
-  static propTypes = {
-    children: PropTypes.func,
-    error: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
-    name: PropTypes.string,
-    onChange: PropTypes.func,
-    set: PropTypes.func,
-    unset: PropTypes.func,
-    value: PropTypes.any,
-  };
-
-  componentDidMount() {
-    const { name, set, value } = this.props;
-
-    if (value !== undefined) {
-      set(name, value);
-    }
-  }
-
-  componentWillUnmount() {
-    const { name, unset } = this.props;
-
-    unset(name);
-  }
-
-  handleChange = after => {
-    const { name, onChange, set, value: before } = this.props;
-
-    onChange(after, before);
-    set(name, after);
-  };
-
-  render() {
-    const { children, error, value } = this.props;
-
-    return children({ error, onChange: this.handleChange, value });
-  }
 }
 
 export class Field extends PureComponent {
@@ -98,7 +53,4 @@ export class Field extends PureComponent {
   }
 }
 
-export default compose(
-  withControlledProp('error', 'onError'),
-  withControlledProp('value', 'onChange'),
-)(Field);
+export default withControlledProp('error', 'onError')(Field);
