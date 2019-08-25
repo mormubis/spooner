143debe894ec7962d973dd0706e9374e65cc2e1d
Adrian de la Rosa <adrian@delarosab.me>
2019-08-25 13:49:53 +0200

    :bug: Adding Error component different values

--- src/Error.js
+++ src/Error.js
@@ -3,25 +3,83 @@ import PropTypes from 'prop-types';
 
 import { useForm } from './Form';
 
-const Error = ({ value: defaultError }) => {
+function wrap(content, wrapper) {
+  return wrapper(content);
+}
+
+const Collection = ({ value }) => (
+  <ul>
+    {value.map(
+      (item, index) =>
+        item && (
+          // eslint-disable-next-line react/no-array-index-key
+          <li key={index}>
+            #{index}: <Error root={false} value={item} />
+          </li>
+        ),
+    )}
+  </ul>
+);
+
+Collection.propTypes = {
+  value: PropTypes.array,
+};
+
+const Literal = ({ value }) => <span>{value}</span>;
+
+Literal.propTypes = {
+  value: PropTypes.string,
+};
+
+const Map = ({ value }) => (
+  <ul>
+    {Object.entries(value).map(([key, item]) => (
+      <li>
+        {key}: <Error root={false} value={item} />
+      </li>
+    ))}
+  </ul>
+);
+
+Map.propTypes = {
+  value: PropTypes.object,
+};
+
+const Root = ({ children }) => <div role="alert">{children}</div>;
+
+Root.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]),
+};
+
+const Nested = ({ children }) => children;
+
+const Error = ({ root = true, value: defaults }) => {
   const state = useForm();
 
-  const error = defaultError || state.error;
+  const value = defaults || state.error || {};
 
-  return (
-    <div role="alert">
-      <ul>
-        {Object.entries(error).map(([key, value]) => (
-          <li key={key}>
-            {key}: {typeof value === 'object' ? <Error value={value} /> : value}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+  let Type;
+
+  switch (true) {
+    case Array.isArray(value):
+      Type = Collection;
+      break;
+    case typeof value === 'object':
+      Type = Map;
+      break;
+    default:
+      Type = Literal;
+      break;
+  }
+
+  return wrap(<Type value={value} />, root ? Root : Nested);
 };
 
 Error.propTypes = {
+  root: PropTypes.bool,
   value: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
 };
 
